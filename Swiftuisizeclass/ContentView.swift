2ce4b7281b9fc295375075e820f205cde38d681b
Bill Mårtensson <bill@magictechnology.se>
2020-11-23 12:35:34 +0100

    Initial Commit

---
+++ Swiftuisizeclass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftuisizeclass
+//
+//  Created by Bill Martensson on 2020-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftuisizeclass/SwiftuisizeclassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuisizeclassApp.swift
+//  Swiftuisizeclass
+//
+//  Created by Bill Martensson on 2020-11-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuisizeclassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
